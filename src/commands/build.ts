240aea99aef95a392790cdb7991b76b415a2d544
bremea <bremeagames@gmail.com>
2025-06-30 22:25:26 -0400

    update debug log

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -68,7 +68,7 @@ export class BuildCommand extends Command {
 			buildTargets.push(targetData);
 		}
 
-		console.log(process.env.REDIS_URL)
+		console.log("redis url: " + process.env.REDIS_URL)
 
 		await interaction.reply({
 			content: `Select Build Target(s):`,
