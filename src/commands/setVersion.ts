7a0b62cb9cb3e94e800d55a9c6503ad6ccc28b52
bremea <bremeagames@gmail.com>
2026-05-10 13:48:26 -0400

    add command to set auto builder version

---
+++ src/commands/setVersion.ts
@@ -0,0 +1,31 @@
+import { Command } from '@sapphire/framework';
+import { setCurrentSemanticVersion } from '../lib/redis';
+
+export class SetVersionCommand extends Command {
+	public constructor(context: Command.LoaderContext, options: Command.Options) {
+		super(context, { ...options, preconditions: ['RoleCheck'] });
+	}
+
+	public override registerApplicationCommands(registry: Command.Registry) {
+		registry.registerChatInputCommand(
+			(builder) =>
+				builder
+					.setName('setver')
+					.setDescription('Set the current semantic version to be used by auto-builder')
+					.addStringOption((option) => option.setName('version').setDescription('New default semantic version').setRequired(true)),
+			{ guildIds: ['1382087628181995771'] }
+		);
+	}
+
+	public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+		const versionString = interaction.options.getString('version', true);
+
+		await setCurrentSemanticVersion(versionString);
+
+		await interaction.reply({
+			content: `✅ Set default semantic version for auto-builder to \`${versionString}\``,
+			withResponse: true
+		});
+		return;
+	}
+}
