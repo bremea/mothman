cb8cfd5c9331d439b3bda646e3fbb6c0c2c1a07b
bremea <bremeagames@gmail.com>
2026-05-21 04:13:07 -0400

    update endpoint to use util function

--- src/routes/time.get.ts
+++ src/routes/time.get.ts
@@ -1,10 +1,8 @@
 import { Route } from '@sapphire/plugin-api';
+import { getEasternTime } from '../lib/utils.ts';
 
 export class VersionRoute extends Route {
 	public async run(request: Route.Request, response: Route.Response) {
-		const easternTime = new Date().toLocaleString('en-US', {
-			timeZone: 'America/New_York'
-		});
-		return easternTime;
+		return getEasternTime();
 	}
 }
